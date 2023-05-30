dd8c377abbf2a1bd561eb77d13fbe4f69aa993d0
hibeenovul <hibeenovul19@gmail.com>
2023-05-30 19:21:17 +0100

    class Square that defines a square by: (based on 2-square.py)

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""Area of a square"""
+
+
 class Square:
-    """ 
+    """
     A class that defines a square by its size
     """
     def __init__(self, size=0):
-        """ 
+        """
         Method to initialize the square object
         """
         if not isinstance(size, int):
@@ -15,7 +18,7 @@ class Square:
             self.__size = size
 
     def area(self):
-        """ 
+        """
         Method that returns the square are of the object
         """
         return (self.__size ** 2)
