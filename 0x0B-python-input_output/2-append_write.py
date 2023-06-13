d9a14150427dbd3c69efb3244b1d5440ab62b359
hibeenovul <hibeenovul19@gmail.com>
2023-06-13 16:09:33 +0100

    function that appends a string at the end of a text file (UTF8) and returns the number of characters added

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,6 +2,7 @@
 """ This module  contains a function that reads n lines of a text file"""
 
 
+
 def read_lines(filename="", nb_lines=0):
     """ Function that reads from a file and prints its number of lines
 
